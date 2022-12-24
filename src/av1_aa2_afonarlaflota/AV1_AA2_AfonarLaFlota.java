ce446fe9f43f113df518f68464eeb0bd0af33a07
JMA CASA <lacasadelaarquitectura@hotmail.com>
2022-12-24 19:40:15 +0100

    v2

--- src/av1_aa2_afonarlaflota/AV1_AA2_AfonarLaFlota.java
+++ src/av1_aa2_afonarlaflota/AV1_AA2_AfonarLaFlota.java
@@ -1,131 +1,198 @@
+// importación de módulos necesarios para el desarrollo de la práctica.
 package av1_aa2_afonarlaflota;
 import java.util.Arrays;
 import java.util.Scanner;
 /**
  *
- * @author Usuario
+ * @author JMA
  */
 public class AV1_AA2_AfonarLaFlota {
 
-    public static char[][]  matriu= new char[11][11];     
-    public static void main(String[] args) {
-        // TODO code application logic here
-        inicio1();
-        inicio2();
-        Scanner scan=new Scanner(System.in);
-        int niv = scan.nextInt();
+// se delcara la matriz principal del tablero    
      
-        niv = nivell(niv);
-        AV1_AA2_AfonarLaFlota.matriu= matriu;
-        crear_tauler_nivell_1();
-        imprimir_matriu();
- //   for (int i = 0; i < tauler.length; i++) {
-// Per a cada columna (nota)
-//for (int j = 0; j < tauler[i].length; j++) {
-//System.out.println(tauler[i][j]);
-//}
-//}
-      
-      
+    
+// se declara la funcion main
+    public static void main(String[] args) {
+// se llama a la funcion inicio 1 que continee el mensaje de bienvenida.        
+        inicio1();
+        
+// se llama a la funcion inicio 2 que contiene la lista de niveles a escoger.        
+        inicio2();
+        
+//se declara el esacaner que nos permite introdcir datos por teclado.
+        Scanner scan=new Scanner(System.in);
+        
+// se pide un dato de entrada al usuario y se llama a la funcion nivell()
+       int niv = scan.nextInt();
+       int nivell =  nivell(niv);
+       
+// se llama a la funcion opcion
+    
+    int[] opcion= opcion(nivell);
+    
+             int files= opcion[0];
+             int columnes= opcion[1];
+             int llanxes= opcion[2];
+             int vaixells= opcion[3];
+             int cuirassats= opcion[4];
+             int portaavions= opcion[5];
+             int max_intents= opcion[6];
+             
+ // se llama a la funcion jugar_partida
+               jugar_partida(files,columnes,llanxes,vaixells,cuirassats,
+portaavions,max_intents);
+               
+               
+// Se llama a la funcion crear_tauler_nivell_1(), que crea el tablero de nivel 1 según parametros de la práctica        
+       // crear_tauler_nivell_1();
+// Se llama a la funcion imprimir_matriu(); que muestra el tablero vacio en pantalla        
+  //      char[][] tauler_de_joc = crear_tauler(files,columnes);        
+   //     imprimir_matriu(tauler_de_joc);
+
+     
         
     }
-    
+// funcion inicio1() muestra el mensaje de bienvenida.      
     public static void inicio1(){
          System.out.println("===== BENVINGUTS A ENFONSAR LA FLOTA =====");
       System.out.println("");
       System.out.println("Nivells de dificultat:");}
-   
+
+// funcion inicio2() muestra los niveles de dificultad disponibles        
     public static void inicio2(){
       System.out.println("1. Fàcil: 5 llanxes, 3 vaixells, 1 cuirassat i 1 portaavions (50 trets).");
       System.out.println("2. Mitjà: 2 llanxes, 1 vaixell, 1 cuirassat i 1 portaavions (30 trets).");
       System.out.println("3. Difícil: 1 llanxa y 1 vaixell (10 trets).");
       System.out.println("Quin nivell tries? ");
     }
-    
+ 
+// funcion repetirNumero() muestra mesaje de error 
      public static void repetirNumero() {
      System.out.println("El nivell escollit no es correcte, torna a triar");
     
     }  
-     
+ 
+// funcion nivell (niv) devuelve el numero del nivel solo si es correcto.  
      public static int nivell (int niv){
       Scanner scan=new Scanner(System.in);
-      while (niv != 1 && niv != 2 && niv != 3){
+      while (niv != 1 && niv != 2 && niv != 3 && niv != 4){
           repetirNumero();
           inicio2();
           niv = scan.nextInt();
       }return niv;
     }
+    
+// función opcion()  
+ public static int[] opcion(int nivell) {
+          
+     Scanner scan=new Scanner(System.in);
+      int[] opcion = new int[7];
      
-     
-     
-     //=======================================
-// MÈTODES DE TAULER
-//=======================================
-//
-// FUNCIÓ: Crear tauler buit amb "-" en totes les poscions
- public static char[][] crear_tauler_nivell_1(){
+     switch(nivell){
+    
+         case 1: opcion= new int[] {11,11,5,3,1,1,50};
+         break;
+         
+         case 2: opcion= new int[] {11,11,2,1,1,1,30};
+         break;
+
+         case 3: opcion= new int[] {11,11,1,1,0,0,10};
+         break;
+
+        case 4: 
+            opcion= new int[7];
+            for (int i = 0; i <opcion.length; ++i) {
+           opcion[i] = scan.nextInt();
+        } break;   
         
-          for (int i = 0; i <11; ++i) {
-            Arrays.fill(matriu[i],'-');
+                } return opcion;
+            }
+     
+// funcion jugar_partida() 
+  public static void jugar_partida(int files, int columnes, int llanxes, int vaixells, int cuirassats, int
+portaavions, int max_intents) {
+    
+     char[][] tauler_de_joc = crear_tauler(files,columnes);
+     imprimir_matriu(tauler_de_joc);
+     
+     
+     
+     
+  }
+  
+//funcion crear_tauler_nivell_1() buit amb "-" en totes les poscions y la numeracion perimetral. devulve un array rellenado segun parametros de la práctica.
+ public static char[][] crear_tauler(int files, int columnes){
+        char[][]  tauler= new char[files][columnes];     
+          for (int i = 0; i <tauler.length; ++i) {
+            Arrays.fill(tauler[i],'-');
 }
           int z=65; 
-          for (int j = 1; j <11; ++j) {
+          for (int j = 1; j <tauler.length; ++j) {
             int asciiValue = z;
             char convertedChar = (char)asciiValue;
-            matriu[j][0] = convertedChar;
+            tauler[j][0] = convertedChar;
             z++;
           }
           
           int w=48; 
-          for (int j = 1; j <11; ++j) {
+          for (int j = 1; j <tauler.length; ++j) {
             int asciiValue = w;
             char convertedChar = (char)asciiValue;
-            matriu[0][j] = convertedChar;
+            tauler[0][j] = convertedChar;
             w++;
           }
-          return matriu;
+          
+          return tauler;
     }
-    
-    public static void imprimir_matriu(){
+  
+ //Funcion aletorio 
+   public static int random(){
+    int aleatori = (int) (1+Math.random()*(11-1+1));
+   return aleatori;
+ }
+  
+//funcion introducir barco
+  
+   
+  
+ // función imprimir_matriu(), imprime el array bidimensional en forma de matriz por pantalla.
+    public static void imprimir_matriu(char[][]tauler_de_joc){
+      /*  int x= 0;
+        int y=65; 
+           
+        for (int i = 0; i <tauler.length; ++i) {
+        System.out.print(x+" ");
+        x++;
         
-           for (int i = 0; i <11; ++i) {
+        }
+        */
+        for (int i = 0; i <tauler_de_joc.length; ++i) {
               System.out.println(" ");
-              for(int j=0; j<11; j++){
-                  System.out.print(matriu[i][j]+" ");
+              for(int j=0; j<tauler_de_joc.length; j++){
+                  System.out.print(tauler_de_joc[i][j]+" ");
                   
               }
           }
     }
-}
-   
-   
-   
-     
-     
-     
-     
-     
-  /* 
-public static char[][] crear_tauler(int files, int columnes) {
-char tauler[][] = new char[files][columnes];
+    
 
-for (int i = 0; i <columnes; ++i) {
-Arrays.fill(tauler[i],'-');
-}
-return tauler;
-} 
+ 
 
-// PROCEDIMENT: Mostra per pantalla el tauler
-public static void mostra_tauler(char[][] tauler, boolean veureTot) {}
+
+ 
+ 
+ 
+ 
+
+ }
+   
+
+   
+   
+   
      
-     public static void mostartablero(){
-        for(int i=0;i<11;i++){
-            for(int o=0;o<11;o++){
-                System.out.print(tauler[i][o]+"    ");
-            }
-          }
-    }    
      
      
-}
-*/
+     
+     
+
